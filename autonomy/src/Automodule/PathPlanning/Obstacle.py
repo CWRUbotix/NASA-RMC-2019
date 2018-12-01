21fcc8244d351c7bec11a5756c5287c4c00dcea6
jsh318900 <jsh318900@gmail.com>
2018-12-01 13:19:57 -0500

    Obstacle class fixed

--- autonomy/src/Automodule/PathPlanning/Obstacle.py
+++ autonomy/src/Automodule/PathPlanning/Obstacle.py
@@ -1,6 +1,7 @@
 import math
+
 class Obstacle:
-    def _inti_(self, center_x, center_y, radius):
+    def __init__(self, center_x, center_y, radius):
         self.center_x = center_x
         self.center_y = center_y
         self.radius = radius
