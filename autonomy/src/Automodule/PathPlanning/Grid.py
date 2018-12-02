a3057b56d8cd2f42a5c60e08cd21d8fcf0582aba
Philip Woolley <philipgmwoolley@gmail.com>
2018-12-02 00:47:29 -0500

    created grid class

---
+++ autonomy/src/Automodule/PathPlanning/Grid.py
@@ -0,0 +1,13 @@
+class Grid:
+
+    def __init__(self, vertices):
+        self.vertices = vertices
+
+    def getVertices(self):
+        return self.vertices
+
+    def removeVertex(self, vertex):
+        self.vertices.remove(vertex)
+
+    def addVertex(self, vertex):
+        self.vertices.append(vertex)
