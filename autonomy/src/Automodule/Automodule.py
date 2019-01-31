f8a26eb9292f7fa01009d352339344571314dc3e
Seohyun <jsh318900@gmail.com>
2019-01-31 16:11:57 -0500

    Initial ROS framework

--- autonomy/src/Automodule/Automodule.py
+++ autonomy/src/Automodule/Automodule.py
@@ -1,5 +1,42 @@
+from collections import deque
+
+import rospy
+
+
 """
 Main Module for Autonomy operation
+"""
 
+
+""" HARD CODED TABLES (dictionary)"""
+interruptible = [4, 8, 11, 14] #Task IDs whose time can be modified
+
+tasks = #Map from ID to time allocated.
+{}
+
+
+"""Routine Builder Stuff"""
 
 """
+Takes in task ID and create list of function calls that
+"""
+def createRoutine(ID):
+	pass
+
+def modifyTimes(timeLeft):
+	pass
+
+def shutdownRoutine():
+	pass
+
+
+def main():
+	rospy.init_node("automodule")
+	rospy.on_shutdown(shutdownRoutine)
+
+	# Initialize RoutineMaker threads here
+
+	rospy.spin()
+	
+
+if __name__ == "__main__": main()
