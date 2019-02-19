957dc3ecd2f3353f5d9148a634629f54a2df0ca7
Anu_U21 <upadhyaanu@gmail.com>
2019-02-19 14:51:30 -0500

    Update Grid.py

--- autonomy/src/Automodule/PathPlanning/Grid.py
+++ autonomy/src/Automodule/PathPlanning/Grid.py
@@ -16,7 +16,7 @@ class Grid:
     def addObstacle(self, obstacle):
         self.obstacles.append(obstacle)
 
-    #returns true if the grid space with lower left vertex at (Xpos, Ypos) is obstructed by an obstacle
+    # returns true if the grid space with lower left vertex at (Xpos, Ypos) is obstructed by an obstacle
     def gridBlocked(self, Xpos, Ypos):
         xBlocked = False
         yBlocked = False
@@ -29,5 +29,5 @@ class Grid:
                 yBlocked = (obstacle.getCenter()[1] - obstacle.getRadius()) < Ypos + 1
             else:
                 yBlocked = (obstacle.getCenter()[1] + obstacle.getRadius()) > Ypos
-        return (xBlocked & yBlocked)
+        return xBlocked & yBlocked
 
