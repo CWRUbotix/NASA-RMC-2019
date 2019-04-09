b59a8b4d2b675ae410b2be086fc49e1904067aa5
Philip Woolley <philipgmwoolley@gmail.com>
2019-04-09 16:00:05 -0400

    tweaking theta star

--- autonomy/src/Automodule/PathPlanning/Grid.py
+++ autonomy/src/Automodule/PathPlanning/Grid.py
@@ -19,19 +19,19 @@ class Grid:
     def addObstacle(self, obstacle):
         self.obstacles.append(obstacle)
 
-    # returns true if the grid space with lower left vertex at (Xpos, Ypos) is obstructed by an obstacle
+    # returns true if the grid space with upper left vertex at (Xpos, Ypos) is obstructed by an obstacle
     @staticmethod
     def gridBlocked(Xpos, Ypos, obstacles, unitScale):
         xBlocked = False
         yBlocked = False
         for obstacle in obstacles:
             if obstacle.center_x >= Xpos:
-                xBlocked = (obstacle.getCenter()[0] - obstacle.getRadius()) < (Xpos + unitScale)
+                xBlocked = ((obstacle.getCenter()[0] - obstacle.getRadius()) < (Xpos + unitScale))
             else:
-                xBlocked = (obstacle.getCenter()[0] + obstacle.getRadius()) > Xpos
+                xBlocked = ((obstacle.getCenter()[0] + obstacle.getRadius()) > Xpos)
             if obstacle.center_y >= Ypos:
-                yBlocked = (obstacle.getCenter()[1] - obstacle.getRadius()) < (Ypos + unitScale)
+                yBlocked = ((obstacle.getCenter()[1] - obstacle.getRadius()) < (Ypos + unitScale))
             else:
-                yBlocked = (obstacle.getCenter()[1] + obstacle.getRadius()) > (Ypos)
+                yBlocked = ((obstacle.getCenter()[1] + obstacle.getRadius()) > Ypos)
         return (xBlocked and yBlocked)
 
--- autonomy/src/Automodule/PathPlanning/testingThetaStar.py
+++ autonomy/src/Automodule/PathPlanning/testingThetaStar.py
@@ -4,56 +4,55 @@ from Obstacle import Obstacle
 from PathPlanning import Position
 from Vertex import Vertex
 import random
-neighbors = []
 
-startpos = Position(5, 5, 0, 1)
-endpos = Position(700, 700, 0, 1)
-start = Vertex(0, 0, 1272, neighbors, startpos)
+startpos = Position(0, 0, 0, 1)
+endpos = Position(400, 400, 0, 1)
+start = Vertex(0, 0, 565, [], startpos)
 start.setParent(start)
-end = Vertex(None, 1272, 0, neighbors, endpos)
+end = Vertex(None, 565, 0, [], endpos)
 
 
 obstacles = []
 positions = []
-vertices=[[0 for x in range(10)] for y in range(10)]
+vertices=[[None]*10 for y in range(10)]
 for i in range(10):
     for j in range(10):
-
-        point = Vertex(None, (i+j)*100, (((9-i)**2 + (9-j)**2)**1/2)*100, [], Position(i*100, j*100, 0, 0))
-        if (i == 7 and j == 7):
+        point = Vertex(None, (((i**2)+(j**2))**1/2)*100, (((4-i)**2 + (4-j)**2)**1/2)*100, [], Position(j*100, i*100, 0, 0))
+        if (i == 4 and j == 4):
             vertices[i][j] = end
-        elif(i==0 & j==0):
+        elif(i==0 and j==0):
             vertices[i][j] = start
         else:
             vertices[i][j] = point
 for i in range(10):
     for j in range(10):
+        elem = vertices[i][j]
         if (i + 1 <= 9):
-            vertices[i][j].addNeighbor(vertices[i + 1][j])
+            elem.addNeighbor(vertices[i + 1][j])
             if (j + 1 <= 9):
                 #vertices[i][j].addNeighbor(vertices[i + 1][j + 1])
-                vertices[i][j].addNeighbor(vertices[i][j + 1])
+                elem.addNeighbor(vertices[i][j + 1])
             if (j - 1 >= 0):
                 #vertices[i][j].addNeighbor(vertices[i + 1][j - 1])
-                vertices[i][j].addNeighbor(vertices[i][j - 1])
+                elem.addNeighbor(vertices[i][j - 1])
         if (i - 1 >= 0):
-            vertices[i][j].addNeighbor(vertices[i - 1][j])
-            if (j + 1 <= 9):
+            elem.addNeighbor(vertices[i - 1][j])
+            #if (j + 1 <= 9):
                 #vertices[i][j].addNeighbor(vertices[i - 1][j + 1])
-                None
-            if (j - 1 >= 0):
+             #   None
+            #if (j - 1 >= 0):
                 #vertices[i][j].addNeighbor(vertices[i - 1][j - 1])
-                None
+             #   None
+        vertices[i][j]=elem
+        positions.append(Position(j * 100, i * 100, 0, 0))
 
-        positions.append(Position(i * 100, j * 100, 0, 0))
-
-vertices[0][0].setParent(start)
+#vertices[0][0].setParent(start)
 for i in range(15):
-    r1 = random.randint(150, 1000)
-    r2 = random.randint(150, 1000)
+    r1 = random.randint(150, 600)
+    r2 = random.randint(150, 600)
     r3 = random.randint(5, 50)
     obstacles.append(Obstacle(r1, r2, r3))
-obstacles.append(Obstacle(400, 400, 5))
+obstacles.append(Obstacle(200, 200, 5))
 path = thetaStar(start, end, obstacles, 1)
 path.printPath()
 drawPath(path, obstacles, positions)
