d5a41366e207ad99ac430e7369f76f3dc3118afa
Steven Xie <stevengxie@gmail.com>
2019-04-15 17:30:40 -0400

    Small change made to fix 'key-down key-up' bounce behavior. README created
    Signed-off-by: Steven Xie <stevengxie@gmail.com>

--- client/src/client/my_module.py
+++ client/src/client/my_module.py
@@ -201,8 +201,9 @@ class MyPlugin(Plugin):
 
     def keyReleaseEvent(self, event):
         if event.key() in (Qt.Key_W, Qt.Key_S, Qt.Key_A, Qt.Key_D):
-            print("Key released")
-            self.set_locomotion_speeds(0,0)
+            if not event.isAutoRepeat():
+                print("Key released")
+                robotInterface.sendDriveCommand(0, 0)
 
     def set_locomotion_speeds(self, port_speed, starboard_speed):
         respPort = robotInterface.sendMotorCommand(0, port_speed)
